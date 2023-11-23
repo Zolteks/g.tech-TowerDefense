8f482e5c23ac51dd48301f34f925b44c84973093
Riuck01 <117281231+Riuck01@users.noreply.github.com>
2023-11-23 17:52:29 +0100

    quit button on menu + restart on menu while lose

--- main.py
+++ main.py
@@ -43,13 +43,21 @@ class Game:
     def updateMenu(self):
         mouseX, mouseY = pyxel.mouse_x, pyxel.mouse_y
         
-        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT) and mouseX > 30 and mouseX < 30+50 and mouseY > 30 and mouseY < 30+10:
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT) and mouseX > 30 and mouseX < 30+41 and mouseY > 30 and mouseY < 30+11:
             self.state = "level"
             self.initLevel()
+
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT) and mouseX > 30 and mouseX < 30+37 and mouseY > 45 and mouseY < 45+11:
+            pyxel.quit()        
+        
+        pyxel.rect(30, 30, 41, 7, 11)
+        pyxel.text(31, 31, "Start Game", 0)
+        pyxel.rect(30, 45, 37, 7, 11)
+        pyxel.text(31, 46, "Quit Game", 0)
     
     def updateLevel(self):
         if self.life < 1:
-            pyxel.quit()
+            self.state = "menu"
         self.enemies.update(self)
         self.bullets.update(self)
         self.turrets.update(self)
@@ -60,6 +68,16 @@ class Game:
     
     def initLevel(self):
         self.enemySpawnCD.reset()
+        self.building = False
+        self.life = 20
+        self.gold = 100
+        self.scenarium = 0
+        self.enemySpawnCD = tweening.TimedBool(60*2)
+        self.enemies = Intellist(50)
+        self.bullets = Intellist(100)
+        self.turrets = Intellist(10)
+        for i in range(7):
+            self.turrets.add(player.TurretSpace(self, 15 + 25*i, 5))
     
     def rect_overlap(self, bullet, enemy):
         return bullet.x < enemy.x + enemy.w and bullet.x + bullet.w > enemy.x and bullet.y < enemy.y + enemy.h and bullet.y + bullet.h > enemy.y
@@ -78,13 +96,20 @@ class Game:
                     self.bullets.delete(curB)
     
     def spawnEnemy(self):
-        enemyType = [normalEnemy,mediumEnemy,bigEnemy]
-        enemyRandomSpawn = random.choice(enemyType)
+        # enemyType = [normalEnemy,mediumEnemy,bigEnemy]
+        # enemyRandomSpawn = random.choice(enemyType)
         enemyW = 10
         enemyH = 10
         spawnX = 30 + enemyW
         spawnY = self.screenH
-        self.enemies.add(enemyRandomSpawn(spawnX, spawnY, enemyW, enemyH, {"xLimit": self.screenW - 40 -10, "yLimit": 40} ))
+        rand = random.randint(1, 100)
+        if rand <= 50:
+            enemyToSpawn = normalEnemy(spawnX, spawnY, enemyW, enemyH, {"xLimit": self.screenW - 40 -10, "yLimit": 40} )
+        elif rand <= 95:
+            enemyToSpawn = mediumEnemy(spawnX, spawnY, enemyW, enemyH, {"xLimit": self.screenW - 40 -10, "yLimit": 40} )
+        elif rand <= 100:
+            enemyToSpawn = bigEnemy(spawnX, spawnY, enemyW, enemyH, {"xLimit": self.screenW - 40 -10, "yLimit": 40} )
+        self.enemies.add(enemyToSpawn)
     
     def draw(self):
         if self.state == "menu":
@@ -96,8 +121,10 @@ class Game:
         
     def drawMenu(self):
         pyxel.cls(0)
-        pyxel.rect(30, 30, 50, 10, 11)
+        pyxel.rect(30, 30, 41, 7, 11)
         pyxel.text(31, 31, "Start Game", 0)
+        pyxel.rect(30, 45, 37, 7, 11)
+        pyxel.text(31, 46, "Quit Game", 0)
     
     def drawLevel(self):
         pyxel.cls(13)
